153be9be609fe26ec6a9f5db546899850778b6f2
JoshuaCWebDeveloper <coosriverjoshua1@outlook.com>
2017-03-02 10:29:56 -0800

    Add new JCFluxStore#Construction test to test if custom flux objects can be passed

--- test/JCFluxStore.js
+++ test/JCFluxStore.js
@@ -1,12 +1,13 @@
 /* JCFluxStore.js
  * Tests JCFluxStore
- * Dependencies: assert modules, JCFluxStore, FluxStore classes, mocha context
+ * Dependencies: assert, flux modules, JCFluxStore, FluxStore classes, mocha context
  * Author: Joshua Carter
  * Created: November 6, 2016
  */
 "use strict";
 //include dependencies
 var assert = require('assert'),
+    Flux = require('flux'),
     JCFluxStore = require('../build/index').JCFluxStore,
     FluxStore = require('../build/index').JCFlux.FluxStore;
 
@@ -34,6 +35,33 @@ describe('JCFluxStore', function () {
         });
         it ('Should instantiate with default flux actions', function () {
             assert(testObj.Actions() && testObj.Actions().update);
+        });
+        it ('Should instantiate with custom flux actions', function () {
+            //create flux actions
+            var testDispatch = new Flux.Dispatcher(),
+                testActions = {
+                    updateCustom: function (id, data, val) {
+                        testDispatch.dispatch({
+                            type: 12,
+                            id: id,
+                            params: [data, val]
+                        });
+                    }
+                },
+                customClass = class extends JCFluxStore {
+                    constructor (data, defaults) {
+                        super (data, defaults, testDispatch, testActions, {UPDATE_CUSTOM: 12});
+                        this.RegisterCustomDispatch();
+                    }
+                    
+                    RegisterCustomDispatch () {
+                        assert(this._AC.UPDATE_CUSTOM == 12);
+                        assert(this._Dispatch == testDispatch);
+                    }
+                },
+                //create instance with custom register method
+                testCustomObj = new customClass(data, defaults);
+            assert(testCustomObj.Actions().updateCustom);
         });
     });
     
